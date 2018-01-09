9c8829284d34fbfc7e716175c9979f0f1942e296
kornilova <kornilova203@gmail.com>
2018-01-09 14:20:03 +0200

    Test numbers parsing

---
+++ src/test/java/com/github/kornilova_l/matlab/parser/MatlabParserTest.kt
@@ -0,0 +1,19 @@
+package com.github.kornilova_l.matlab.parser
+
+import com.github.kornilova_l.matlab.MatlabParserDefinition
+import com.intellij.testFramework.ParsingTestCase
+
+open class MatlabParserTest : ParsingTestCase("", "m", MatlabParserDefinition()) {
+
+    override fun getTestDataPath(): String {
+        return "src/test/resources/parser"
+    }
+
+    override fun includeRanges(): Boolean {
+        return true
+    }
+
+    fun testValidNumbers() {
+        doTest(true)
+    }
+}
