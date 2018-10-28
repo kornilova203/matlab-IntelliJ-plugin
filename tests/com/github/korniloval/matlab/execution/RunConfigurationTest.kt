a1d1b836b7f72d5e41b162a101c17a0c3f587b57
Liudmila Kornilova <liudmila.kornilova@jetbrains.com>
2018-10-28 15:10:28 +0300

    Use JPS

---
+++ tests/com/github/korniloval/matlab/execution/RunConfigurationTest.kt
@@ -0,0 +1,58 @@
+package com.github.korniloval.matlab.execution
+
+import com.github.korniloval.matlab.execution.MatlabCannotRunException
+import com.intellij.execution.executors.DefaultRunExecutor
+import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.util.ThrowableRunnable
+import junit.framework.TestCase
+
+class RunConfigurationTest : RunConfigurationTestCase() {
+
+    private val fileContent = """
+                |function [] = hello()
+                |    fprintf("hello, world\n")
+                |end
+                """.trimMargin()
+
+    private val interpreterPath = "/usr/local/bin/octave"
+
+    fun testRunConfigurationFromFile() {
+        val myFile = myFixture.configureByText("hello.m", fileContent)
+        val configuration = createConfiguration(myFile)
+
+        TestCase.assertNotNull(configuration)
+        configuration ?: return
+        TestCase.assertNull(configuration.getInterpreterPath())
+        TestCase.assertEquals(myFile.virtualFile.canonicalPath, configuration.getFilePath())
+    }
+
+    fun testSetInterpreterPath() {
+        val myFile = myFixture.configureByText("hello.m", fileContent)
+        val configuration = createConfiguration(myFile)
+
+        TestCase.assertNotNull(configuration)
+        configuration ?: return
+
+        TestCase.assertNull(configuration.getInterpreterPath())
+
+        configuration.setInterpreterPath(interpreterPath)
+
+        assertInterpreterPath(createConfiguration(myFile), interpreterPath)
+
+        val myFile2 = myFixture.configureByText("hello2.m", fileContent)
+        assertInterpreterPath(createConfiguration(myFile2), interpreterPath)
+    }
+
+    fun testRunnerExceptions() {
+        val myFile = myFixture.configureByText("hello.m", fileContent)
+        val configuration = createConfiguration(myFile)!!
+
+        val executor = DefaultRunExecutor.getRunExecutorInstance()
+        val executionEnvironment = ExecutionEnvironment()
+        assertThrows(MatlabCannotRunException::class.java,
+                "Matlab interpreter is not specified.",
+                ThrowableRunnable<MatlabCannotRunException> { configuration.getState(executor, executionEnvironment) })
+
+        configuration.setInterpreterPath(interpreterPath)
+    }
+}
---
+++ tests/com/github/korniloval/matlab/resolve/FindUsagesTest.kt
@@ -0,0 +1,18 @@
+package com.github.korniloval.matlab.resolve
+
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase
+
+/**
+ * @author Liudmila Kornilova
+ **/
+class FindUsagesTest : LightPlatformCodeInsightFixtureTestCase() {
+    override fun getTestDataPath(): String = "testData/findusages/"
+
+    fun testVariable() = doTest(1)
+    fun testFunction() = doTest(1)
+
+    private fun doTest(usagesCount: Int) {
+        val usages = myFixture.testFindUsages(getTestName(false) + ".m")
+        assertEquals(usagesCount, usages.size)
+    }
+}
