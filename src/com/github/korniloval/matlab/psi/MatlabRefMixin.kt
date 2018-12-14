aeed14f14dcd1fb9916af848abb9379407f1bb0a
Liudmila Kornilova <liudmila.kornilova@jetbrains.com>
2018-12-14 19:55:23 +0300

    Remove excessive recovery rules

--- src/com/github/korniloval/matlab/psi/MatlabRefMixin.kt
+++ src/com/github/korniloval/matlab/psi/MatlabRefMixin.kt
@@ -2,6 +2,7 @@ package com.github.korniloval.matlab.psi
 
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiNameIdentifierOwner
+import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.search.SearchScope
 import com.intellij.psi.tree.IElementType
 
@@ -9,20 +10,19 @@ import com.intellij.psi.tree.IElementType
  * @author Liudmila Kornilova
  **/
 abstract class MatlabRefMixin(elementType: IElementType) : MatlabCompositePsiElement(elementType), MatlabRef, PsiNameIdentifierOwner {
-
     override fun setName(name: String): PsiElement {
         val ref = MatlabPsiUtil.createRefFromText(project, name)
         identifier.replace(ref.identifier)
         return this
     }
 
-    override fun getUseScope(): SearchScope {
-        return super.getUseScope()
-    }
-
     override fun getReference(): MatlabReference? {
         return MatlabReference(this)
     }
+
+    override fun getResolveScope(): GlobalSearchScope = GlobalSearchScope.projectScope(project)
+
+    override fun getUseScope(): SearchScope = GlobalSearchScope.projectScope(project)
 
     override fun getName(): String? = text
 
