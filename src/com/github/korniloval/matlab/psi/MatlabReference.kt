ec2d7a922c9e9f4e84b3e7f4fab3ec30cdf41659
Liudmila Kornilova <liudmila.kornilova@jetbrains.com>
2019-02-15 15:39:20 +0300

    Resolve with cache

--- src/com/github/korniloval/matlab/psi/MatlabReference.kt
+++ src/com/github/korniloval/matlab/psi/MatlabReference.kt
@@ -3,14 +3,26 @@ package com.github.korniloval.matlab.psi
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiReferenceBase
 import com.intellij.psi.ResolveState
+import com.intellij.psi.impl.source.resolve.ResolveCache
 import com.intellij.psi.util.PsiTreeUtil
 
 /**
  * @author Liudmila Kornilova
  **/
 class MatlabReference(myElement: MatlabRefExpr) : PsiReferenceBase<MatlabRefExpr>(myElement) {
+    companion object {
+        private const val USE_CACHE = true
+        private val RESOLVER = ResolveCache.AbstractResolver { ref: MatlabReference, _: Boolean -> ref.resolveInner() }
+    }
 
-    override fun resolve(): MatlabDeclaration? = resolveInner()
+    override fun resolve(): MatlabDeclaration? {
+        return if (USE_CACHE) {
+            val cache = ResolveCache.getInstance(element.project)
+            cache.resolveWithCaching(this, RESOLVER, false, false)
+        } else {
+            resolveInner()
+        }
+    }
 
     private fun resolveInner(): MatlabDeclaration? {
         val processor = MatlabResolvingScopeProcessor(this)
