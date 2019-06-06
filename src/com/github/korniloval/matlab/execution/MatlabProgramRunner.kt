ad13ae87d907d493000980928f20ec9b52dd7aaf
Liudmila Kornilova <liudmila.kornilova@jetbrains.com>
2019-06-06 10:15:20 +0300

    Execute only matlab run configurations

--- src/com/github/korniloval/matlab/execution/MatlabProgramRunner.kt
+++ src/com/github/korniloval/matlab/execution/MatlabProgramRunner.kt
@@ -5,7 +5,10 @@ import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.execution.runners.DefaultProgramRunner
 
 class MatlabProgramRunner : DefaultProgramRunner() {
-    override fun canRun(executorId: String, profile: RunProfile): Boolean = DefaultRunExecutor.EXECUTOR_ID == executorId
+    override fun canRun(executorId: String, profile: RunProfile): Boolean {
+        return DefaultRunExecutor.EXECUTOR_ID == executorId &&
+                profile is MatlabRunConfiguration
+    }
 
     override fun getRunnerId(): String = MATLAB_PROGRAM_RUNNER_ID
 
