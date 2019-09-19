654888095dfa84549776e81f5018b8cf23be9ef9
Liudmila Kornilova <kornilova203@gmail.com>
2019-09-19 19:26:22 +0300

    Retrieve variables values

--- src/com/github/korniloval/matlab/debug/MatlabStack.kt
+++ src/com/github/korniloval/matlab/debug/MatlabStack.kt
@@ -23,8 +23,14 @@ class Frame internal constructor(private val xPosition: XSourcePosition, private
                 val matchResult = VAR_DESC_PATTERN.matchEntire(text) ?: return
                 val attr = matchResult.groups["attr"]?.value ?: ""
                 if (attr.contains(AUTO_VAR_ATTR)) return
-                val type = matchResult.groups["class"]?.value
-                matchResult.groups["name"]?.value?.let { children.add(MatlabValue(it, type)) }
+                val size1 = matchResult.groups["size1"]?.value?.toInt()
+                val size2 = matchResult.groups["size2"]?.value?.toInt()
+                var type = matchResult.groups["class"]?.value
+                if (type != null && size1 != null && size2 != null) {
+                    if (size1 == 1 && size2 > 1) type = "vector<$type>"
+                    else if (size1 > 1 && size2 > 1) type = "matrix<$type>"
+                }
+                matchResult.groups["name"]?.value?.let { children.add(MatlabValue(it, type, debugProcess)) }
             }
 
             override fun end() {
@@ -34,13 +40,31 @@ class Frame internal constructor(private val xPosition: XSourcePosition, private
     }
 
     companion object {
-        private val VAR_DESC_PATTERN = Regex("\\s+(?<attr>\\w+)?\\s+(?<name>\\w+)\\s+(?<size>\\d+x\\d+)\\s+(?<bytes>\\d+)\\s+(?<class>\\w+)\n")
+        private val VAR_DESC_PATTERN = Regex("\\s+(?<attr>\\w+)?\\s+(?<name>\\w+)\\s+(?<size1>\\d)+x(?<size2>\\d+)\\s+(?<bytes>\\d+)\\s+(?<class>\\w+)\n")
         private const val AUTO_VAR_ATTR = 'a'
     }
 }
 
-class MatlabValue(name: String, private val type: String?) : XNamedValue(name) {
+class MatlabValue(name: String, private val type: String?, private val debugProcess: MatlabDebugProcess) : XNamedValue(name) {
     override fun computePresentation(node: XValueNode, place: XValuePlace) {
-        node.setPresentation(AllIcons.Nodes.Variable, type, "", false)
+        debugProcess.commandsQueue.queue.add(object : Command(name) {
+            private val sb = StringBuilder()
+            private val prefix = Regex("^$name =\\s+")
+            private var first = true
+
+            override fun process(text: String) {
+                if (first) {
+                    first = false
+                    val res = prefix.find(text)
+                    if (res != null) sb.append(text.substring(res.value.length))
+                    else sb.append(text)
+                }
+                else sb.append(text)
+            }
+
+            override fun end() {
+                node.setPresentation(AllIcons.Nodes.Variable, type, sb.toString().trim(), false)
+            }
+        })
     }
 }
