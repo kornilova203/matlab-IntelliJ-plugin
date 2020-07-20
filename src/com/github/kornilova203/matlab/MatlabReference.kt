8665b03b511888cee2a58e057036c008391e101b
Aleksandra Dedovich <aleksandra.dedovich@jetbrains.com>
2020-07-20 21:26:05 +0300

    Add multifile resolve

--- src/com/github/kornilova203/matlab/MatlabReference.kt
+++ src/com/github/kornilova203/matlab/MatlabReference.kt
@@ -3,10 +3,15 @@ package com.github.kornilova203.matlab
 import com.github.kornilova203.matlab.psi.*
 import com.github.kornilova203.matlab.stub.MatlabClassDeclarationIndex
 import com.github.kornilova203.matlab.stub.MatlabFunctionDeclarationIndex
-import com.intellij.psi.*
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiReferenceBase
+import com.intellij.psi.ResolveState
 import com.intellij.psi.impl.source.resolve.ResolveCache
+import com.intellij.psi.scope.PsiScopeProcessor
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.stubs.StubIndex
+import com.intellij.psi.stubs.StubIndexKey
 import com.intellij.psi.util.PsiTreeUtil
 
 /**
@@ -30,28 +35,28 @@ class MatlabReference(myElement: MatlabRefExpr) : PsiReferenceBase<MatlabRefExpr
     private fun resolveInner(): MatlabDeclaration? {
         val processor = MatlabResolvingScopeProcessor(this)
         val containingFile = myElement.containingFile
-        val project = myElement.project
         PsiTreeUtil.treeWalkUp(processor, myElement, containingFile, ResolveState.initial())
         if (isResolved(processor)) {
             return processor.declaration
         }
-        val functionDeclarations = StubIndex.getElements(MatlabFunctionDeclarationIndex.KEY, myElement.text, project, GlobalSearchScope.projectScope(project), MatlabFunctionDeclaration::class.java)
-        for (functionDeclaration in functionDeclarations) {
-            if (functionDeclaration.containingFile != containingFile) {
-                processor.execute(functionDeclaration, ResolveState.initial())
-            }
-        }
-        val classDeclarations = StubIndex.getElements(MatlabClassDeclarationIndex.KEY, myElement.text, project, GlobalSearchScope.projectScope(project), MatlabClassDeclaration::class.java)
-        for (classDeclaration in classDeclarations) {
-            if (classDeclaration.containingFile != containingFile) {
-                processor.execute(classDeclaration, ResolveState.initial())
-            }
-        }
+        resolveIndex(processor, MatlabFunctionDeclarationIndex.KEY, MatlabFunctionDeclaration::class.java)
+        resolveIndex(processor, MatlabClassDeclarationIndex.KEY, MatlabClassDeclaration::class.java)
         return if (processor.declaration == myElement) null else processor.declaration
     }
 
     private fun isResolved(processor: MatlabResolvingScopeProcessor): Boolean {
         return processor.declaration != null && processor.declaration != myElement
+    }
+
+    private fun <Psi : PsiElement?> resolveIndex(processor: PsiScopeProcessor, indexKey: StubIndexKey<String, Psi>, requiredClass : Class<Psi>) {
+        val psiElements = StubIndex.getElements(indexKey, myElement.text, myElement.project, GlobalSearchScope.projectScope(myElement.project), requiredClass)
+        for (psiElement in psiElements) {
+            if (psiElement != null) {
+                if (psiElement.containingFile != myElement.containingFile) {
+                    processor.execute(psiElement, ResolveState.initial())
+                }
+            }
+        }
     }
 
     override fun getVariants(): Array<PsiElement> = emptyArray()
