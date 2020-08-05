892c53969f4d066671c9a340d9442fe1c28ad0e1
Aleksandra Dedovich <aleksandra.dedovich@jetbrains.com>
2020-08-05 14:52:33 +0300

    Add control expressions

---
+++ gen/com/github/kornilova203/matlab/psi/impl/MatlabControlExprImpl.java
@@ -0,0 +1,28 @@
+// This is a generated file. Not intended for manual editing.
+package com.github.kornilova203.matlab.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static com.github.kornilova203.matlab.psi.MatlabTypes.*;
+import com.github.kornilova203.matlab.psi.*;
+
+public class MatlabControlExprImpl extends MatlabExprImpl implements MatlabControlExpr {
+
+  public MatlabControlExprImpl(@NotNull ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull MatlabVisitor visitor) {
+    visitor.visitControlExpr(this);
+  }
+
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof MatlabVisitor) accept((MatlabVisitor)visitor);
+    else super.accept(visitor);
+  }
+
+}
